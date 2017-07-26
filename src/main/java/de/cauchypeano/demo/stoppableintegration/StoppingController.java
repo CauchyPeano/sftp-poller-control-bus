ef9841e01c1a9e2bd9ae9fa54c3f66724fee0057
Igor Konoplyanko <igor.konoplyanko@wirecard.com>
2017-07-26 13:32:39 +0200

    getting status of adapter

--- src/main/java/de/cauchypeano/demo/stoppableintegration/StoppingController.java
+++ src/main/java/de/cauchypeano/demo/stoppableintegration/StoppingController.java
@@ -2,7 +2,10 @@ package de.cauchypeano.demo.stoppableintegration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.http.HttpStatus;
+import org.springframework.integration.endpoint.SourcePollingChannelAdapter;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +14,18 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@DependsOn("fileFlow")
 public class StoppingController {
 
     @Autowired
     @Qualifier("controlChannel")
     private MessageChannel controlChannel;
+
+    @Autowired
+    private AbstractApplicationContext abstractApplicationContext;
+
+    @Autowired
+    @Qualifier("sftpInboundAdapter") SourcePollingChannelAdapter sftpInboundAdapter;
 
     @RequestMapping(value = "/stop", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.ACCEPTED)
@@ -33,6 +43,13 @@ public class StoppingController {
         controlChannel.send(new GenericMessage<>("@sftpInboundAdapter.start()"));
 
         return "started...";
+    }
+
+    @RequestMapping(value = "/poller", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String getIsRunning() {
+
+        return "status: " + sftpInboundAdapter.isRunning();
     }
 
 }
